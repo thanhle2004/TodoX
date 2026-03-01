5c5bd8d5c496796b3a60e5ca3961c10b68876ff2
thanhle2004 <yatomisuo@gmail.com>
2026-03-01 18:42:18 +0700

    Ready to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,20 +3,32 @@ import tasksRouter from './routes/tasksRouters.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 const app = express();
 
 
 //middleware
 app.use(express.json());
-app.use(cors({origin: 'http://localhost:5173'})); // Allow requests from the frontend
 
+if(process.env.NODE_ENV !== "production") {
+    app.use(cors({origin: 'http://localhost:5173'})); // Allow requests from the frontend
+}
 
-app.use("/api/tasks", tasksRouter)
+app.use("/api/tasks", tasksRouter);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
